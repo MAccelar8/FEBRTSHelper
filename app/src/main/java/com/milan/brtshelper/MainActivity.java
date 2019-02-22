2bc0f45c5c4e55ff8cd4e717bf66c856376818e9
Milan Lakhani <lakhanimilan9@gmail.com>
2019-02-22 23:34:33 +0700

    Initial commit

---
+++ app/src/main/java/com/milan/brtshelper/MainActivity.java
@@ -0,0 +1,37 @@
+package com.milan.brtshelper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onmainButtonclicked(View view){
+
+
+    }
+
+    public void onEntersourceMainClicked(View view){
+
+
+    }
+
+    public void onDestinationMainClicked(View view){
+
+    }
+
+    public void onSearchbar1click(View view){
+        Intent i = new Intent(this, SearchSouceFromMain.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/milan/brtshelper/SearchSouceFromMain.java
@@ -0,0 +1,14 @@
+package com.milan.brtshelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchSouceFromMain extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_source_from_main);
+    }
+
+
+}
