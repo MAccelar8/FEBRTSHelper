2255e075766c8cfc4eebf9e4276c1c86bc6400ee
Milan Lakhani <lakhanimilan9@gmail.com>
2019-02-23 18:44:04 +0530

    commit : Icon added

--- app/src/main/java/com/milan/brtshelper/MainActivity.java
+++ app/src/main/java/com/milan/brtshelper/MainActivity.java
@@ -1,23 +1,78 @@
 package com.milan.brtshelper;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    android.widget.SearchView searchviewmain;
+    android.widget.SearchView searchviewdestmain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        searchviewdestmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_dest);
+//
+//        searchviewdestmain.setOnSearchClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, v ,"src_shared_searchbar_main");
+//                Intent i = new Intent(MainActivity.this,SearchSouceFromMain.class);
+//                startActivityForResult(i,2);
+//            }
+//        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestcode, int resultcode, Intent data) {
+        Log.i("hghgg", "Hardik");
+        if (requestcode == 2) {
+            if (resultcode == RESULT_OK) {
+                Log.i("hghgg", "Hardik222222222222222");
+                searchviewdestmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_dest);
+                String temp = (String) data.getExtras().getString("passsrc");
+                Log.i("112222222", temp);
+                searchviewdestmain.setIconifiedByDefault(true);
+                searchviewdestmain.setFocusable(true);
+                searchviewdestmain.setIconified(false);
+//                searchviewmain.requestFocus();
+                searchviewdestmain.requestFocusFromTouch();
+                searchviewdestmain.setQuery(temp, false);
+                searchviewdestmain.clearFocus();
+
+            }
+        }
+        if (requestcode == 1) {
+            if (resultcode == RESULT_OK) {
+                Log.i("hghgg", "Hardik1111111111111111");
+                searchviewmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_src);
+                String temp = (String) data.getExtras().getString("passsrc");
+                Log.i("11hghgg", temp);
+                searchviewmain.setIconifiedByDefault(true);
+                searchviewmain.setFocusable(true);
+                searchviewmain.setIconified(false);
+//                searchviewmain.requestFocus();
+                searchviewmain.requestFocusFromTouch();
+                searchviewmain.setQuery(temp, false);
+                searchviewmain.clearFocus();
+
+            }
+
+        }
+
     }
 
-    public void onmainButtonclicked(View view){
 
+
+    public void onmainButtonclicked(View view){
 
     }
 
@@ -31,7 +86,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSearchbar1click(View view){
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, view ,"src_shared_searchbar_main");
         Intent i = new Intent(this, SearchSouceFromMain.class);
-        startActivity(i);
+        startActivityForResult(i,1);
+
+    }
+    public void onSearchbar2click(View view){
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, view ,"src_shared_searchbar_main");
+        Intent i = new Intent(this, SearchSouceFromMain.class);
+        startActivityForResult(i,2);
+
     }
 }
--- app/src/main/java/com/milan/brtshelper/SearchSouceFromMain.java
+++ app/src/main/java/com/milan/brtshelper/SearchSouceFromMain.java
@@ -1,13 +1,77 @@
 package com.milan.brtshelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchSouceFromMain extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_source_from_main);
+        final android.widget.SearchView searchview_sfma = (android.widget.SearchView) findViewById(R.id.searchbar_sfma_src);
+        searchview_sfma.setIconifiedByDefault(true);
+        searchview_sfma.setFocusable(true);
+        searchview_sfma.setIconified(false);
+//                searchviewmain.requestFocus();
+        searchview_sfma.requestFocusFromTouch();
+
+        final List<String> listofstation = new ArrayList<String>();
+        ListView searchlistsrc = findViewById(R.id.listview_src);
+        listofstation.add("Nehrunagar");
+        listofstation.add("Swaminarayan Mandir");
+        listofstation.add("Little Wings");
+
+        final ArrayAdapter<String> arraysrcadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listofstation);
+
+        searchlistsrc.setAdapter(arraysrcadapter);
+        searchview_sfma.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (listofstation.contains(query)) {
+                    arraysrcadapter.getFilter().filter(query);
+
+                }else {
+                    //no match found
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arraysrcadapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        searchlistsrc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = (String) parent.getItemAtPosition(position);
+                if (searchview_sfma.hasFocus()){
+                    Intent resultintent = new Intent();
+                    resultintent.putExtra("passsrc",str);
+                    setResult(Activity.RESULT_OK,resultintent);
+                    Log.i("itemcl","Click sdadasd");
+                    finishAfterTransition();
+                }
+            }
+        });
+
     }
 
 
