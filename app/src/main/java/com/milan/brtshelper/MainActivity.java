588fd6d6edd1d1070db7710f81740f473ae28cb9
Milan Lakhani <lakhanimilan9@gmail.com>
2019-02-24 08:09:16 +0530

    commit : FireBase Dependencies added. Support for Databse, Cloud,Auth,Data Analysis ,etc. SH1 auth2 key retrieved and added to Firebase.

--- app/src/main/java/com/milan/brtshelper/MainActivity.java
+++ app/src/main/java/com/milan/brtshelper/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
 //                startActivityForResult(i,2);
 //            }
 //        });
+        EditText editt = findViewById(R.id.editText);
+        editt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, v ,"src_shared_searchbar_main");
+                Intent i = new Intent(MainActivity.this, SearchSouceFromMain.class);
+                startActivityForResult(i,1);
+                return false;
+            }
+        });
 
     }
     @Override
@@ -40,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
                 searchviewdestmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_dest);
                 String temp = (String) data.getExtras().getString("passsrc");
                 Log.i("112222222", temp);
+
+
                 searchviewdestmain.setIconifiedByDefault(true);
                 searchviewdestmain.setFocusable(true);
                 searchviewdestmain.setIconified(false);
@@ -52,17 +67,24 @@ public class MainActivity extends AppCompatActivity {
         }
         if (requestcode == 1) {
             if (resultcode == RESULT_OK) {
-                Log.i("hghgg", "Hardik1111111111111111");
-                searchviewmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_src);
-                String temp = (String) data.getExtras().getString("passsrc");
-                Log.i("11hghgg", temp);
-                searchviewmain.setIconifiedByDefault(true);
-                searchviewmain.setFocusable(true);
-                searchviewmain.setIconified(false);
-//                searchviewmain.requestFocus();
-                searchviewmain.requestFocusFromTouch();
-                searchviewmain.setQuery(temp, false);
-                searchviewmain.clearFocus();
+//                Log.i("hghgg", "Hardik1111111111111111");
+//                searchviewmain = (android.widget.SearchView) findViewById(R.id.searchbar_main_src);
+//
+//
+              String temp = (String) data.getExtras().getString("passsrc");
+//                Log.i("11hghgg", temp);
+//                searchviewmain.setIconifiedByDefault(true);
+//                searchviewmain.setFocusable(true);
+//                searchviewmain.setIconified(false);
+
+                EditText editt = findViewById(R.id.editText);
+                editt.setText(temp,EditText.BufferType.EDITABLE);
+                editt.clearFocus();
+
+////                searchviewmain.requestFocus();
+//                searchviewmain.requestFocusFromTouch();
+//                searchviewmain.setQuery(temp, false);
+//                searchviewmain.clearFocus();
 
             }
 
@@ -72,9 +94,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void onmainButtonclicked(View view){
-
-    }
 
     public void onEntersourceMainClicked(View view){
 
@@ -84,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
     public void onDestinationMainClicked(View view){
 
     }
+
 
     public void onSearchbar1click(View view){
 
